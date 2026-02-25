e3b379fce365337627bf8a6c18a84ccdb3705e30
sinokadev <sinoka@sinoka.dev>
2026-02-26 01:37:34 +0900

    basic object scene game

---
+++ pge.py
@@ -0,0 +1,55 @@
+import pygame
+
+pygame.init()
+
+class Object:
+    def __init__(self):
+        pass
+
+    def update(self, event, deltatime):
+        pass
+
+    def draw(self, screen: pygame.Surface):
+        pass
+
+class Scene:
+    def __init__(self):
+        self.color: pygame.Color = pygame.Color(113, 199, 245)
+
+        self.objects: list[Object] = []
+    
+    def update_and_draw(self, screen: pygame.Surface, event, deltatime):
+        screen.fill(self.color)
+
+        for gameobject in self.objects:
+            gameobject.draw()
+            gameobject.update(event, deltatime)
+
+class Game:
+    def __init__(self):
+        self.screen_size: tuple = (960, 640)
+        self.is_running: bool = True
+
+        self.screen: pygame.Surface = pygame.display.set_mode(self.screen_size)
+        self.clock: pygame.time.Clock = pygame.time.Clock()
+
+        self.scenes: list[Scene] = []
+
+    def run(self):
+        while self.is_running:
+            dt = self.clock.tick(100) / 1000
+            events = pygame.event.get()
+            
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+            
+            for scene in self.scenes:
+                scene.update_and_draw(self.screen, events, dt)
+            
+            pygame.display.update()
+    
+    def regist_scene(self, scene: Scene):
+        self.scenes.append(scene)
+
+        return self
---
+++ test.py
@@ -0,0 +1,7 @@
+import pge
+
+scene = pge.Scene()
+
+game = pge.Game().regist_scene(scene)
+
+game.run()
